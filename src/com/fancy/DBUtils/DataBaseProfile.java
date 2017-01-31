107028de53c9ef16e31d94d6ef84c2ff01b65e8d
movie14 <wangjie.hdu@icloud.com>
2017-01-31 16:22:45 +0800

    不知道为什么写这类

---
+++ src/com/fancy/DBUtils/DataBaseProfile.java
@@ -0,0 +1,24 @@
+package com.fancy.DBUtils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Jackie on 2017/1/31.
+ *
+ */
+
+@Component
+public class DataBaseProfile {
+
+    public static final String usename = " ";
+
+    public static final String password = " ";
+
+    public static final String url = " " ;
+
+    public static final int maxConnections = 20 ;
+
+    public static final String drive = " " ;
+
+
+}
