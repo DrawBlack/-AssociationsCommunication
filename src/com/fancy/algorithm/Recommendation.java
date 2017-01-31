6445533aa31d292868de52293cd5ebaa308dede4
movie14 <wangjie.hdu@icloud.com>
2017-01-31 16:24:55 +0800

    最后一个commit 手好酸 ，去打麻将咯

--- src/com/fancy/algorithm/Recommendation.java
+++ src/com/fancy/algorithm/Recommendation.java
@@ -1,7 +1,31 @@
 package com.fancy.algorithm;
 
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * Created by Jackie on 2017/1/31.
+ * interface Recommendation for sort Comparables
  */
-public interface Recommendation {
+
+@Component
+public class Recommendation {
+
+    /**
+     *
+     * @param comparables many no sorted comparables
+     * @param sort a algoorithm for sort comparables
+     * @return sorted comparables
+     */
+
+    public List<Comparable> recommendation(List<Comparable> comparables , Sort sort){
+
+        if (null == comparables || sort == comparables)
+            return sort.sort(comparables);
+
+
+        return null;
+    }
+
 }
---
+++ src/com/fancy/algorithm/Sort.java
@@ -0,0 +1,20 @@
+package com.fancy.algorithm;
+
+import java.util.List;
+
+/**
+ * Created by Jackie on 2017/1/31.
+ * a algorithm for soet comparables
+ */
+
+public interface Sort {
+
+    /**
+     *
+     * @param noSortComparables
+     * @return
+     */
+
+    List<Comparable> sort( List<Comparable> noSortComparables );
+
+}
