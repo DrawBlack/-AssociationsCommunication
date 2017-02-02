bda25ce41d12010953b83cf5bd93bf4776b35f09
movie14 <wangjie.hdu@icloud.com>
2017-02-02 19:18:10 +0800

    spring 配置类

---
+++ src/com/fancy/Configuration.java
@@ -0,0 +1,14 @@
+package com.fancy;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Jackie on 2017/1/31.
+ *
+ *
+ */
+
+@ComponentScan(basePackages = "")
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+}
